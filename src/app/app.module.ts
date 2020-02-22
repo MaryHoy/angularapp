2c7523b95986d74a3fdfb5d17f730c1a2b6ecb28
MaryHoy <55214178+MaryHoy@users.noreply.github.com>
2020-02-21 16:33:26 -0800

    added routing and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,36 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { QuestionFormComponent } from './question-form/question-form.component';
+import { ResultsComponent } from './results/results.component';
+import { RouterModule, Routes } from '@angular/router';
+import { QuestionsComponent } from './questions/questions.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { WelcomeComponent } from './welcome/welcome.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+
+const appRoutes: Routes = [
+  { path: 'welcome', component: WelcomeComponent },
+  { path: ':quizId', component: QuestionsComponent },
+  { path: '', redirectTo: "welcome", pathMatch: "prefix" },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuestionFormComponent
+    QuestionFormComponent,
+    ResultsComponent,
+    QuestionsComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes),
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
